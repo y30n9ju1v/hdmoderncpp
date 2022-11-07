e54c2c20bc5533ad902057f2da88f17879fa116f
ecourse <smkang@codenuri.co.kr>
2022-11-07 16:42:43 +0900

    Add files via upload

--- DAY1/17_static_if1.cpp
+++ DAY1/17_static_if1.cpp
@@ -4,11 +4,20 @@
 #include <type_traits>
 
 // std::is_pointer<T>::value : T 가 포인터 인지 조사하는 기술
-//							  목요일날 배우게 됩니다.(C++11 기술)
+							  //목요일날 배우게 됩니다.(C++11 기술)
+
+
 template<typename T> 
 void foo(T a)
 {
-	if ( std::is_pointer<T>::value )
+	// 기존의 if : 실행시간 제어문
+	//		      컴파일 시간에 결과가 false 가 되어도
+	//		      "템플릿" => "C++함수" 로 만들때 코드가 포함됨.
+//	if ( std::is_pointer<T>::value )
+
+	// C++17 의 "static if" : 조건이 false 라면
+	//						 템플릿 으로 부터 생성되는 함수에는 포함안함
+	if constexpr (std::is_pointer<T>::value)
 	{
 		std::cout << "포인터" << std::endl;
 
@@ -20,6 +29,7 @@ void foo(T a)
 		std::cout << "포인터 아님" << std::endl;
 	}
 }
+
 int main()
 {
 	int n = 0;
