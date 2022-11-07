9cad14de9857974b188dfdf48c330b758520f2df
ecourse <smkang@codenuri.co.kr>
2022-11-07 11:17:33 +0900

    Add files via upload

---
+++ DAY1/5_range_for1.cpp
@@ -0,0 +1,38 @@
+// 5_range_for1.cpp   21 page
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	// 전통적인 for 문
+	for (int i = 0; i < v.size(); i++)
+	{
+	}
+
+	// C++11 에서 새로 나온 for 문
+	// 파이썬 : for n in v
+	// C#    : foreach int n in v
+	for (int n : v)
+	{
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
