c533899ac376e42320de2ed0c8236914fc634c7c
ecourse <smkang@codenuri.co.kr>
2022-11-07 11:29:54 +0900

    Add files via upload

---
+++ DAY1/5_range_for4.cpp
@@ -0,0 +1,29 @@
+// 5_range_for4 - 1번 복사해오세요
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	for (int n : v)
+	{
+		std::cout << n << ", ";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
