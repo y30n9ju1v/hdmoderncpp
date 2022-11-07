5b69f91ef5a250acdeb56ad32ac85aba6ceca105
ecourse <smkang@codenuri.co.kr>
2022-11-07 14:21:47 +0900

    Add files via upload

--- DAY1/10_user_define_literal1.cpp
+++ DAY1/10_user_define_literal1.cpp
@@ -8,7 +8,12 @@ public:
 
 // 34 page 참고
 // operator"" 함수의 인자타입은 약속되어 있습니다.
-Meter operator""m(unsigned long long n)
+
+// 사용자가 만드는 것 : "_"로 시작해야 합니다.
+//					 "_" 로 시작하지 않는 것은 C++ 표준에서 예약되어 
+//					있습니다.
+
+Meter operator""_m(unsigned long long n)
 {
 	Meter m( static_cast<int>(n) );
 	return m;
@@ -19,7 +24,7 @@ int main()
 	double d = 3.4; // "3.4" 는 double 타입 입니다.
 	float f = 3.4f; // "3.4f" 는 float 타입 입니다.
 
-	Meter me = 3m;	// operator""m(3) 을 찾게 됩니다.
+	Meter me = 3_m;	// operator""_m(3) 을 찾게 됩니다.
 
 //	Gram  g = 3g;
 }
