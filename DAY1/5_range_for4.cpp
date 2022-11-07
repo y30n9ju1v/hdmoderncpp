476999e42c2a1c5a2668ed6847ae56dba3ba9679
ecourse <smkang@codenuri.co.kr>
2022-11-07 11:40:43 +0900

    Add files via upload

--- DAY1/5_range_for4.cpp
+++ DAY1/5_range_for4.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 
 // 아래 클래스를 잘 생각해 보세요
+// => C++20 부터 등장하는 "시각(View)" 입니다.
+// => C++20 "Range" 라이브러리의 원리
 template<typename T>
 class reverse_view
 {
@@ -13,12 +15,15 @@ public:
 	auto begin() { return range.rbegin(); } // 핵심!!!
 	auto end()   { return range.rend(); }
 };
-
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5 };
 
-	for (int n : v)
+	reverse_view rv(v);  // C++17 부터 템플릿 인자 생략 가능합니다.
+
+//	for (int n : v)
+//	for (int n : rv)
+	for (int n : reverse_view(v) ) // 임시객체를 생성하는 표기법
 	{
 		std::cout << n << ", ";
 	}
