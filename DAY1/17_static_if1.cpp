34de847fd16c14026385e03aa557ad791d1fe3d2
ecourse <smkang@codenuri.co.kr>
2022-11-07 16:34:17 +0900

    Add files via upload

---
+++ DAY1/17_static_if1.cpp
@@ -0,0 +1,25 @@
+// 17_static_if - 51 page
+
+#include <iostream>
+#include <type_traits>
+
+// std::is_pointer<T>::value : T 가 포인터 인지 조사하는 기술
+//							  목요일날 배우게 됩니다.(C++11 기술)
+template<typename T> 
+void foo(T a)
+{
+	if (std::is_pointer<T>::value)
+	{
+		std::cout << "포인터" << std::endl;
+	}
+	else
+	{
+		std::cout << "포인터 아님" << std::endl;
+	}
+}
+int main()
+{
+	int n = 0;
+	foo(n);
+}
+
