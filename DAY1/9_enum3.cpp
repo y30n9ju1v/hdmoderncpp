e12238ef68c2ec8a152d32603a66a00467a58b11
ecourse <smkang@codenuri.co.kr>
2022-11-07 14:11:55 +0900

    Add files via upload

--- DAY1/9_enum3.cpp
+++ DAY1/9_enum3.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <type_traits>
 
-enum class DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+// 새로운 enum 은 내부적으로 사용하는 타입을 변경할수 있습니다.
+
+//enum class DAYOFWEEK { SUN = 0, MON = 1, TUE = 2 };
+
+enum class DAYOFWEEK : char { SUN = 0, MON = 1, TUE = 2 };
 
 int main()
 {
+	// DAYOFWEEK enum 이 내부적으로 사용하는 타입
 	std::cout << typeid(std::underlying_type_t<DAYOFWEEK>).name()
 			  << std::endl;
 }
