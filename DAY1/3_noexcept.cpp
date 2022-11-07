3522adc5a55adf55add1ff2421a1f8ed1731e307
ecourse <smkang@codenuri.co.kr>
2022-11-07 10:49:14 +0900

    Add files via upload

--- DAY1/3_noexcept.cpp
+++ DAY1/3_noexcept.cpp
@@ -18,14 +18,20 @@ void hoo()
 int main()
 {
 	// noexcept 의미 2. 함수가 예외가 있는지 조사
-	bool b1 = noexcept(foo());
-	bool b2 = noexcept(goo());
-	bool b3 = noexcept(hoo());
-
-	std::cout << b1 << std::endl;
-	std::cout << b2 << std::endl;
-	std::cout << b3 << std::endl;
+	bool b1 = noexcept(foo()); // false
+	bool b2 = noexcept(goo()); // true
+	bool b3 = noexcept(hoo()); // false
+	 
+	std::cout << b1 << std::endl; // 0
+	std::cout << b2 << std::endl; // 1
+	std::cout << b3 << std::endl; // 0
 }
+// 함수가 예외가 없다면 되도록 "noexcept" 를 붙이세요
+// 1. 컴파일러 최적화가 더 잘지원 됩니다.
+// 2. noexcept 가 꼭 필요한 경우가 있습니다. - move, swap, 소멸자
+//										내일 배웁니다.
+							// 단, 소멸자는 noexcept를 붙이지 않아도
+							// 디폴트가 noexcept 입니다.
 
 
 
