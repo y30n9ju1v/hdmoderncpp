221c7c58dd16544544bea7daeba93466a72d3ed1
ecourse <smkang@codenuri.co.kr>
2022-11-07 15:36:07 +0900

    Add files via upload

--- DAY1/14_override2.cpp
+++ DAY1/14_override2.cpp
@@ -3,7 +3,9 @@
 template<typename T> class Base
 {
 public:
-	virtual void foo(const T a)
+	// 아래 코드는 "a" 가 const 입니다.
+	//virtual void foo(const T a) // 이 코드와
+	virtual void foo(T const  a)  // 이 코드는 동일합니다.
 	{
 		std::cout << "Base foo" << std::endl;
 	}
@@ -12,7 +14,14 @@ class Derived : public Base<int*>
 {
 public:
 	// 가상함수 재정의시 override 붙이지 말고 해보세요.
+	// 아래 코드는 "a" 는 const 가 아닙니다.
+	// "a 가 가리키는 곳"이 const 입니다.
+	//virtual void foo(const int* a) 
 
+	virtual void foo( int* const a)
+	{
+		std::cout << "Derived foo" << std::endl;
+	}
 };
 
 int main()
