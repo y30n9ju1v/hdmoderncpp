6163299e3faa9cebb2e37db92c888a1e16f3b7d5
ecourse <smkang@codenuri.co.kr>
2022-11-08 10:17:54 +0900

    Add files via upload

---
+++ DAY2/3_초기화4-2.cpp
@@ -0,0 +1,14 @@
+
+// std::cin 은 istream 클래스의 객체 입니다.
+// (정확히는 basic_istream)
+class istream
+{
+public:
+	bool fail() { return false; } // 입력실패에 따라 결과반환
+};
+istream cin;
+
+int main()
+{
+	if (cin) {}
+}
