96d9b61939fcedfd51e93d39629fb078550ae6f0
ecourse <smkang@codenuri.co.kr>
2022-11-08 13:46:18 +0900

    Add files via upload

---
+++ DAY2/4_타입추론3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// 규칙 2. 템플릿 파라미터가 "참조(T& a)" 일때
+// => 함수 인자가 가진 "reference 속성만 제거" 한후 T의 타입결정
+// => const volatile 은 유지
+template<typename T> void foo(T& a)
+{
+	std::cout << typeid(T).name() << std::endl; 
+			// => 잘못된 결과, reference 를 출력 못합니다.
+}
+
+int main()
+{
+	int n = 10;
+	int& r = n;
+	const int c = 10;
+	const int& cr = c;
+
+	foo(n);  // T = int			a = int&
+	foo(c);  // T = const int	a = const int&
+	foo(r);  // T = int			a = int&
+	foo(cr); // T = const int   a = const int&
+}
