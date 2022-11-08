33cd5286fa0383ee83887484ee1a6577a89b79dc
ecourse <smkang@codenuri.co.kr>
2022-11-08 16:35:00 +0900

    Add files via upload

--- DAY2/5_임시객체3-1.cpp
+++ DAY2/5_임시객체3-1.cpp
@@ -10,7 +10,6 @@ const T& max(const T& a, const T& b)
 {
 	return a < b ? b : a;
 }
-
 int main()
 {
 	int n = max(3, 4);
@@ -22,6 +21,12 @@ int main()
 	// ret1은 결국 s2를 가리킵니다.
 	// 항상 안전 합니다.
 
-	const std::string& ret2 = max(std::string("aaa"), std::string("bbb"));
+	const std::string& ret2 = max(std::string("aaa"),
+								  std::string("bbb"));
 	// ret2는 안전할까요 ?
+	// ret2 는 "dangling reference" 입니다. 사용하면 안됩니다.
+	// 임시객체의 수명연장은 "최초의 참조" 한번만 가능합니다.
+
+	// cppreference.com 에서 "std::max" 검색해 보세요.
+	// "Notes" 읽어 보세요.
 }
