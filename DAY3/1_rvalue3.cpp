8c3b77a5182ed756e00fb6afaa383f16d7d829e7
ecourse <smkang@codenuri.co.kr>
2022-11-09 09:59:50 +0900

    Add files via upload

--- DAY3/1_rvalue3.cpp
+++ DAY3/1_rvalue3.cpp
@@ -5,18 +5,12 @@
 // decltype(표현식) : 표현식이 lvalue 가 될수 있다면 참조타입으로 결정
 //					        lvalue 가 될수 없다면 값타입으로 결정
 
-#define print_value_category( ... )	\
-	if ( std::is_lvalue_reference<decltype(( __VA_ARGS__ ))>::value )
-		std::cout << "lvalue" << std::endl;
-	else
-		std::cout << "rvalue" << std::endl;	
-
 int main()
 {
 	int n = 10;
 
 
-	if ( std::is_lvalue_reference<decltype(( n ))>::value )
+	if ( std::is_lvalue_reference<decltype( 5 )>::value )
 		std::cout << "lvalue" << std::endl;
 	else
 		std::cout << "rvalue" << std::endl;
