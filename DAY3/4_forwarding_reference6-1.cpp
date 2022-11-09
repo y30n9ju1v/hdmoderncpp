0a04c27b06dc9ef3018967330c3991c8ba00db0d
ecourse <smkang@codenuri.co.kr>
2022-11-09 16:26:56 +0900

    Add files via upload

--- DAY3/4_forwarding_reference6-1.cpp
+++ DAY3/4_forwarding_reference6-1.cpp
@@ -41,14 +41,12 @@ T&& xmove(T&& obj)
 	return static_cast<T&&>(obj);
 }
 
-
-
 int main()
 {
 	Object o1;
 	Object o2 = o1;				// copy
 	Object o3 = foo();			// move
-	Object o4 = xmove(o1);		// move
+	Object o4 = xmove(o1);		// move => copy ¹ß»ý  
 	Object o5 = xmove(foo());	// move
 
 
