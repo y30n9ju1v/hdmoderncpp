1285e1a374087a479c0158b42e5b20bf13ab6a00
ecourse <smkang@codenuri.co.kr>
2022-11-09 11:50:45 +0900

    Add files via upload

--- DAY3/3_move01.cpp
+++ DAY3/3_move01.cpp
@@ -9,11 +9,13 @@ int main()
 	std::string s1 = "hello";
 	std::string s2 = "hello";
 
-	std::string s3 = s1;
+	std::string s3 = s1;	// 이 순간 s1의 문자열을 s3가 깊은 복사.
 	std::string s4 = std::move(s2);
+							// s2의 문자열을 s4로 이동
+							// s2 는 자원 없음
 
-	std::cout << s1 << std::endl; 
-	std::cout << s2 << std::endl;
+	std::cout << s1 << std::endl;// "hello"
+	std::cout << s2 << std::endl;// ""
 
 
 }
