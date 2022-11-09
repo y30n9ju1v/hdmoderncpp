f25f940e830025ba9043133da474b53df365ddae
ecourse <smkang@codenuri.co.kr>
2022-11-09 16:15:57 +0900

    Add files via upload

--- DAY3/4_forwarding_reference6.cpp
+++ DAY3/4_forwarding_reference6.cpp
@@ -18,11 +18,18 @@ public:
 	}
 };
 
+// C++표준 함수는 "std::move()" 를 구현해 봅시다.
+template<typename T>
+T&& xmove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+
 int main()
 {
 	Object o1;
-	Object o2 = o1; 
-	Object o3 = std::move(o1); 
+	Object o2 = o1;			// copy
+	Object o3 = xmove(o1);	// move
 
 }
 
