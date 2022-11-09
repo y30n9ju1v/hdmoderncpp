bdc0ba883069d4b1d6809f90ea2e8606588dcfea
ecourse <smkang@codenuri.co.kr>
2022-11-09 13:52:40 +0900

    Add files via upload

--- DAY3/3_move10-1.cpp
+++ DAY3/3_move10-1.cpp
@@ -19,7 +19,15 @@ public:
 	}
 	// move 생성자에서는 모든 멤버를 "std::move()" 로 옮겨라!
 	// 아래 코드에서 "noexcept" 를 어떻게 해야 할까요 ?
-	Object(Object&& other) noexcept 
+
+	// void f1() noexcept       : 예외가 없다.
+	// void f1() noexcept(true) : 예외가 없다.
+	// void f1() noexcept(false): 예외가 있다.
+
+	// string : move 생성자에 예외 없습니다.(cppreference.com 참고)
+	// T      : 알수 없습니다. 아래 처럼 조사해야 합니다.
+	Object(Object&& other) 
+		noexcept ( std::is_nothrow_move_constructible<T>::value  )
 		: id(other.id), 
 		  name(std::move(other.name)),
 		  data(std::move(other.data))
@@ -27,6 +35,7 @@ public:
 		std::cout << "move" << std::endl;
 	}
 };
+
 
 int main()
 {
