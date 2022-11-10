98f3fa2d13a637dfc9dba9486d2a914784dc7c98
ecourse <smkang@codenuri.co.kr>
2022-11-10 15:19:06 +0900

    Add files via upload

--- DAY4/6_함수객체5-1.cpp
+++ DAY4/6_함수객체5-1.cpp
@@ -26,9 +26,17 @@ int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
 	
-	show_function_object sfo;
+	show_function_object  sfo;
+	show_function_object2 sfo2;
 
-	my_for_each(v.begin(), v.end(), sfo );
-		// my_for_each(iterator, iterator, ? ) 의 함수 생성
+	my_for_each(v.begin(), v.end(), sfo ); // for_each(show_function_object  );
+	my_for_each(v.begin(), v.end(), sfo2); // for_each(show_function_object2  );
+
+	// 함수가 아닌 함수 객체를 사용하면
+	// 위처럼 다른 타입의 함수객체를 보내면
+
+	// 단점 : 여러개의 for_each 함수가 생성된다. 코드 메모리 증가
+	// 장점 : operator() 가 인라인 치환 됩니다. 속도가 빨라 집니다.
+	
 }
 
