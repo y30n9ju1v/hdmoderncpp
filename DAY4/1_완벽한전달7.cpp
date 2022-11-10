b4d1af347249a8405b481d1bdd2f3ae97cb7f010
ecourse <smkang@codenuri.co.kr>
2022-11-10 10:30:14 +0900

    Add files via upload

--- DAY4/1_완벽한전달7.cpp
+++ DAY4/1_완벽한전달7.cpp
@@ -14,9 +14,17 @@ template<typename T>
 void foo(T&& arg)
 {
 	// 아래 2개 캐스팅의 차이를 정확히 생각해 보세요.
-	static_cast<T&&>(arg);
-	static_cast<std::remove_reference_t<T>&&>(arg);
+	// 1. foo 의 인자로 lvalue, rvalue 를 보낸것에 따라 다른 캐스팅
+	//    lvalue 를 보내면 lvalue 캐스팅
+	//    rvalue 를 보내면 rvalue 캐스팅
+	// => std::forward()
+	Object o1 = static_cast<T&&>(arg);
+
+	// 2. 인자의 value 속성에 상관없이 무조건 rvalue 로 캐스팅
+	// => std::move()의 원리
+	Object o2 = static_cast<std::remove_reference_t<T>&&>(arg);
 }
+
 int main()
 {
 	Object obj;
