15a4090758c9e8320d4ce066f61c1b993add280e
ecourse <smkang@codenuri.co.kr>
2022-11-10 10:19:06 +0900

    Add files via upload

--- DAY4/1_완벽한전달6.cpp
+++ DAY4/1_완벽한전달6.cpp
@@ -25,17 +25,29 @@ template<typename F> void chronometry(F f, int&& arg)
 template<typename F, typename T> 
 void chronometry(F f, T&& arg)
 {
-	f(static_cast<T&&>(arg));
+	// 핵심 1. 아래 캐스팅은 절대 "rvalue 캐스팅"이 아닙니다.
+	//        chronometry(함수, n)  : static_cast<int&>(arg)
+	//        chronometry(함수, 10) : static_cast<int&&>(arg)
+
+	// 즉, 
+	// (인자로) lvalue 를 (전달하면) lvalue 로 캐스팅하고
+	//         rvalue 를 (전달하면  받으면서 lvalue 가된 것을 ) 
+	//								다시 rvalue 로 캐스팅 합니다.
+//	f(static_cast<T&&>(arg));
+
+	f(std::forward<T>(arg)); // 이 함수가 하는 일이
+							// 위 캐스팅 입니다.
+							// "T&&" 가 아니라 "T" 로 전달하면됩니다.
 }
 
 int main()
 {
 	int n = 0;
-	chronometry(goo, n);	// T=?,  T&&=?
-							// 함수 모양 : chronometry(F f, ? )
-							// 캐스팅 모양: static_cast<?>(arg);
+	chronometry(goo, n);	// T=int&,  T&&=int& && => int&
+							// 함수 모양 : chronometry(F f, int& )
+							// 캐스팅 모양: static_cast<int&>(arg);
 
-	chronometry(foo, 10);	// T=?,  T&&=?
-							// 함수 모양 : chronometry(F f, ? )
-							// 캐스팅 모양: static_cast<?>(arg);
+	chronometry(foo, 10);	// T=int,  T&&=int&&
+							// 함수 모양 : chronometry(F f, int&& )
+							// 캐스팅 모양: static_cast<int&&>(arg);
 }
