ecb12c1f59c55af45f44ad1cc55a89421dceedcd
ecourse <smkang@codenuri.co.kr>
2022-11-10 14:46:43 +0900

    Add files via upload

--- DAY4/6_함수객체5.cpp
+++ DAY4/6_함수객체5.cpp
@@ -7,11 +7,22 @@ void show1(int n)
 	std::cout << n << std::endl;
 }
 
+// STL 의 for_each 를 직접 만들어 봅시다.
+template<typename T, typename F>
+void my_for_each(T first, T last, F f)
+{
+	while (first != last)
+	{
+		f(*first);
+		++first;
+	}
+}
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
 	
-	std::for_each(v.begin(), v.end(), show1);
+	my_for_each(v.begin(), v.end(), show1);
 
 }
 
