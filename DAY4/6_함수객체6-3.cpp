4ea85749d15316420582a4fd588e2ff2c2cee254
ecourse <smkang@codenuri.co.kr>
2022-11-10 16:14:11 +0900

    Add files via upload

--- DAY4/6_함수객체6-3.cpp
+++ DAY4/6_함수객체6-3.cpp
@@ -1,24 +1,29 @@
 
-struct Point
+namespace Graphic
 {
-	int x, y;
-};
-
-Point operator+(const Point& p1, const Point& p2)
-{
-	Point p3;
-	p3.x = p1.x + p2.x;
-	p3.y = p1.y + p2.y;
-	return p3;
+	struct Point
+	{
+		int x, y;
+	};
+	Point operator+(const Point& p1, const Point& p2)
+	{
+		Point p3;
+		p3.x = p1.x + p2.x;
+		p3.y = p1.y + p2.y;
+		return p3;
+	}
 }
 
 int main()
 {
-	Point p1 = { 0,0 };
-	Point p2 = { 1,1 };
+	Graphic::Point p1 = { 0,0 };
+	Graphic::Point p2 = { 1,1 };
 
-	operator+(p1, p2); // 함수 이름을 직접 사용해도 되지만
-	p1 + p2;		   // 이렇게 해도 됩니다.
+//	operator+(p1, p2); // 함수 이름을 직접 사용해도 되지만
+//	p1 + p2;		   // 이렇게 해도 됩니다.
+
+	operator+(p1, p2);			 // ok. ADL 때문에
+	Graphics::operator+(p1, p2); // ADL 이 없다면 이렇게 하면 됩니다.
 	 
 }
 
