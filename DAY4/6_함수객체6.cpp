036428abd3a2f34f64857667be461acaf58d3c9c
ecourse <smkang@codenuri.co.kr>
2022-11-10 15:35:32 +0900

    Add files via upload

--- DAY4/6_함수객체6.cpp
+++ DAY4/6_함수객체6.cpp
@@ -5,10 +5,14 @@
 // 함수 객체 장점 3. ADL 규칙이 적용되지 않는다.
 //				=> 장점 보다는 특징!
 
+// ADL : Argument Dependent Lookup
+// => 함수를 찾을때 인자가 속한 namespace 는 자동검색 된다.
+
 int main()
 {
-	int ret1 = max(1, 2);
-	int ret2 = std::max(1, 2);
+//	int ret1 = max(1, 2);		// error. max 는 
+								// "std::" 안에 있습니다.
+	int ret2 = std::max(1, 2);	// ok. 
 
 	std::string s1 = "a";
 	std::string s2 = "n";
