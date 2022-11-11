f665c313a10dca9bc3e4ba0adbf1a961a0e23966
ecourse <smkang@codenuri.co.kr>
2022-11-11 13:25:16 +0900

    Add files via upload

--- DAY5/5_type_traits2.cpp
+++ DAY5/5_type_traits2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <type_traits>
+#include <type_traits> // 이 안에 수십개의 traits 가 있습니다.
 
 // type traits
 // => 타입에 대한 다양한 특성(특질)을 조사하는 기술
@@ -9,10 +9,19 @@ template<typename T> struct is_pointer
 {
 	static constexpr bool value = false;
 };
+// 핵심 : 조건을 만족하는 타입을 위한 "부분 특수화"를 작성하고
+//       value = true
+template<typename T> struct is_pointer<T*>
+{
+	static constexpr bool value = true;
+};
 
 template<typename T> void foo(const T& a)
 {
-	if ( is_pointer<T>::value )
+	// 현재 T 는 "int", "int*"
+//	if ( is_pointer<T>::value )
+	if ( std::is_pointer<T>::value)
+
 		std::cout << "포인터" << std::endl;
 	else
 		std::cout << "포인터 아님" << std::endl;
