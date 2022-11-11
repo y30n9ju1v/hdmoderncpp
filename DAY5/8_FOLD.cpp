168a5c33e5473a09b7d4f05decde4851afd0c9ae
ecourse <smkang@codenuri.co.kr>
2022-11-11 15:14:00 +0900

    Add files via upload

--- DAY5/8_FOLD.cpp
+++ DAY5/8_FOLD.cpp
@@ -11,14 +11,20 @@ int Sum(Types ... args)
 {
 	// ( pack이름  이항연산자 ...)
 //	int s = (args + ... ); // (1 + ( 2 + 3) ) => unary right fold
-	int s = (...  + args); // ((1 + 2) + 3) ) => unary left fold
+//	int s = (...  + args); // ((1 + 2) + 3) ) => unary left fold
 		  
 
+//	int s = (args + ... + 0); // (1+(2+(3+0))) => binary right fold
+	int s = (0 + ... + args); // (((0+1)+2)+3) => binary left fold
+
+	// 핵심 : fold expression 은 위처럼 4개의 표기법이 있습니다
+	
 	return s;
 }
 int main()
 {
-	int n = Sum(1, 2, 3);
+//	int n = Sum(1, 2, 3);
+	int n = Sum(); 
 
 	std::cout << n << std::endl;
 }
