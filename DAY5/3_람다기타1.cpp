ab53886d8a61f27a90c9a15662665948b9bf6955
ecourse <smkang@codenuri.co.kr>
2022-11-11 10:35:04 +0900

    Add files via upload

--- DAY5/3_람다기타1.cpp
+++ DAY5/3_람다기타1.cpp
@@ -6,10 +6,22 @@ public:
 		return a + b; 
 	}
 
+	// 함수 포인터로의 변환을 지원하기 위해
+	// operator() 와 구현이 동일한 static 멤버 함수도 컴파일러가 생성
+	static inline int helper(int a, int b) 
+	{
+		return a + b;
+	}
+
 	// 함수포인터로의 변환을 위한 변환 연산자
 	using FP = int(*)(int, int);
 
-	operator FP() { return 함수 주소; }
+//	operator FP() { return &CompilerGeneratedName::operator(); }
+				// => error 입니다.
+				// => 일반 함수 포인터는 "멤버 함수 주소"와 호환 안됩니다.
+				// => static 멤버 함수로 해야 합니다.
+
+	operator FP() { return &CompilerGeneratedName::helper; }
 };
 
 
