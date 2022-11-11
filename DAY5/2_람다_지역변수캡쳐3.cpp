13fbba565c62407d0c99840b91403bf63e2d0076
ecourse <smkang@codenuri.co.kr>
2022-11-11 10:06:41 +0900

    Add files via upload

---
+++ DAY5/2_람다_지역변수캡쳐3.cpp
@@ -0,0 +1,41 @@
+// 6_람다_지역변수캡쳐1 - 146
+#include <iostream>
+
+int main()
+{
+	int v1 = 10, v2 = 10;
+
+
+	auto f1 = [v1, v2](int a) { v1 = 100; return a + v1 + v2; }; 
+
+	std::cout << v1 << std::endl; 
+
+
+
+	/*
+	// 람다의 지역변수 캡쳐의 원리
+	class CompilerGeneratedName
+	{
+		int v1;
+		int v2;
+	public:
+		CompilerGeneratedName(int a, int b) : v1(a), v2(b) {}
+
+		inline auto operator()(int a) // const
+		{
+			v1 = 100; // error
+					  // 단, mutable 람다 표현식이라면 const 멤버함수가아니므로
+						// 에러 아님
+
+			return a + v1 + v2
+		}
+	};
+
+	auto f1 = CompilerGeneratedName(v1, v2);
+	*/
+}
+
+
+
+
+
