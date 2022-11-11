316f447808a01ae4c924801500d5ba0123f04e2a
ecourse <smkang@codenuri.co.kr>
2022-11-11 16:26:26 +0900

    Add files via upload

--- DAY5/xvalue.cpp
+++ DAY5/xvalue.cpp
@@ -14,9 +14,14 @@ int main()
 
 	// 아래 3줄의 차이점을 생각해 봅시다.
 	// 1. copy ? move 관점
-	// 2. 객체의 파괴 시점
-	//									1			2
-	std::string s3 = s1;			//
-	std::string s4 = std::move(s2);	//
-	std::string s5 = foo();			//
+	// 2. 우변 객체의 파괴 시점
+	//									1		2
+	std::string s3 = s1;			// copy		{} 벗어 날때
+	std::string s4 = std::move(s2);	// move		{} 벗어 날때
+	std::string s5 = foo();			// move		문장의 끝
+
 }
+
+
+//		lvalue				xvalue				prvalue(pure rvalue)
+//						    std::move()된객체		임시객체
