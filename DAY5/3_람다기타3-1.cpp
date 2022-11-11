522ef16d0267167110d69c526bc76ab012bd43ce
ecourse <smkang@codenuri.co.kr>
2022-11-11 11:24:34 +0900

    Add files via upload

--- DAY5/3_람다기타3-1.cpp
+++ DAY5/3_람다기타3-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-template<typename T, typename Dx = std::default_delete<T> > class unique_ptr
+template<typename T, 
+	     typename Dx = std::default_delete<T> > class unique_ptr
 {
 	T* ptr;
 public:
@@ -9,11 +10,25 @@ public:
 
 	~unique_ptr()
 	{
+		// delete ptr 을 하지 않고
+		// 템플릿 2번째 인자로 받은 함수 객체를 사용합니다.
 		Dx d;	
 		d(ptr);	
 	}
 };
+// free 를 수행하는 함수 객체
+struct Freer
+{
+	inline void operator()(void* p) const noexcept
+	{
+		std::cout << "free" << std::endl;
+		free(p);
+	}
+};
+
 int main()
 {
-	unique_ptr<int> up(new int);
+//	unique_ptr<int> up(new int);
+
+	unique_ptr<int, Freer> up( (int*)malloc(40) );
 }
