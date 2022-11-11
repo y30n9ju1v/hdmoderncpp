a6e80ac239a63d358fe6df4de966e1522660196d
ecourse <smkang@codenuri.co.kr>
2022-11-11 14:48:20 +0900

    Add files via upload

--- DAY5/6_가변인자템플릿6.cpp
+++ DAY5/6_가변인자템플릿6.cpp
@@ -4,16 +4,20 @@
 
 // 1. 1번째 인자는 독립된 변수에 담으세요
 
-void foo() {}
+void foo() {} // C++14 까지에서 재귀의 종료를 위해
 
 template<typename T, typename ... Types>
 void foo(T value, Types ... args)
 {
 	std::cout << value << std::endl;
 
-	foo(args...);	// foo(3.4, 'A')
+//	foo(args...);	// foo(3.4, 'A')
 					// foo('A')
 					// foo()
+
+	// C++17 이후에는 아래 처럼 하세요
+	if constexpr (sizeof...(args) > 0)
+		foo(args...);
 
 }
 
